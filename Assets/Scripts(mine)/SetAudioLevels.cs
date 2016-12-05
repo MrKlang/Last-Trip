4d443e3a6c7c5674dea4136a015a2c2e9e463b9f
Olaf Rogowski <orogow@gmail.com>
2016-12-05 03:02:41 +0100

    MainMenu
    Guess what? We got ourselves some main menu stuff. (about half is done)

---
+++ Assets/Scripts(mine)/SetAudioLevels.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudioLevels : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+
+    public void SetMusicLevel(float musicLvl)
+    {
+        mainMixer.SetFloat("musicVol",musicLvl);
+    }
+
+    public void SetSfxLevel(float sfxLvl)
+    {
+        mainMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMasterLevel(float masterLvl)
+    {
+        mainMixer.SetFloat("masterVol", masterLvl);
+    }
+
+    public void SetNarratorLevel(float narratorLvl)
+    {
+        mainMixer.SetFloat("narratorVol", narratorLvl);
+    }
+}
